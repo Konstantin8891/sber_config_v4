a4281a6df69e7f44bcb38c8a58f8fee87b8a61e4
Konstantin8891 <89670253660@mail.ru>
2022-12-09 13:58:05 +0300

    Put endpoint test

--- tests/test_routes/test_main.py
+++ tests/test_routes/test_main.py
@@ -12,18 +12,7 @@ from models import Service, ServiceKey, ServiceVersion
 from schemas import CreateService, Key
 
 
-# def test_create_service(client, db: Session = Depends(get_db)):
 def test_create_service(client):
-    data = {
-        "name": "string",
-        "version": "string",
-        "is_used": True,
-        "keys": [{
-            "service_key": "string",
-            "service_value": "string"
-        }]
-    }
-    # db = client.get_db()
     key = Key(service_key='testkey1', service_value='testvalue1')
     service = CreateService(
         name="testname1",
@@ -33,16 +22,26 @@ def test_create_service(client):
     )
     # response = client.post("/create_service", json.dumps(data))
     # response = client.post("/create_service", service)
-    # print(service.dict())
     # response = client.post("/create_service", params={"service": service.dict()})
     response = client.post("/create_service", json=service.dict())
 
     assert response.status_code == 201
-    assert db.query(Service).filter(Service.name == 'testname1').exists() is True
-    assert db.query(ServiceVersion).filter(ServiceVersion.version == 'testversion1').exists() is True
-    assert db.query(ServiceKey).filter(ServiceKey.service_key == 'testkey1').exists() is True
-    
- 
+    assert client.get(
+        '/?service=testname1&version=testversion1'
+    ).status_code == 200
+
+
+def test_put_config(client):
+    key = Key(service_key='testkey1', service_value='testvalue1')
+    response = client.put('/', params={
+        "service": "testname1",
+        "version": "testversion1",
+        "is_used": True,
+        "keys": [key, ]
+    })
+
+    assert response.status_code == 200
+
 
 def test_get_all(client):
     response = client.get('/get_all_services')
